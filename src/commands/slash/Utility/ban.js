ca4f186367429845988b2a2c7750ec5b520b81b0
Blowad <blask3107@gmail.com>
2023-09-03 21:15:52 +0700

    update slash command ban to kick member demo

--- src/commands/slash/Utility/ban.js
+++ src/commands/slash/Utility/ban.js
@@ -8,32 +8,34 @@ module.exports = {
         .addUserOption((opt) =>
             opt.setName('user')
                 .setDescription('Tên người cần ban')
-                .setRequired(false)
-        )
-        .addIntegerOption((id) =>
-            id.setName('id-user')
-                .setDescription('ID người dùng cần ban')
-                .setRequired(false)
+                .setRequired(true)
         ),
     /**
      * @param {ExtendedClient} client 
      * @param {ChatInputCommandInteraction} interaction 
      */
     run: async (client, interaction) => {
-        const user =  interaction.options.getUser('user');
+
+        // const user =  interaction.options.getUser('user');
         
         const member = interaction.guild.members.cache.get(user.id);
 
+        if (member) {
+            try {
+                const user = await member.kick();
+                await interaction.reply(`<@${user.id}> đã bị đá khỏi server!`);
+            } catch (err) {
+                console.log(err);
+                await interaction.reply("Không thể đá thằng này ra! :(");
+            }
+        } else {
+            await interaction.reply("Không tìm thấy người dùng!");
+        }
+        
 
-        if (!member || !user) {
-            await interaction.reply({
-                content: 'Người dùng không khả dụng!'
-            });
-        };
-
-        await interaction.reply({
-            content: `${user.username}`
-        });
+        // await interaction.reply({
+        //     content: `${user.username}`
+        // });
 
     }
 };
